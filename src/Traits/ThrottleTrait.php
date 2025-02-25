d0eba5a68d8b10bcb4207eb539dbc46ecf2531c6
Florin Motoc <dev@mflorin.com>
2025-02-25 16:59:46 +0200

    ThrottleTrait support for custom $seconds; defaults to 50s

--- src/Traits/ThrottleTrait.php
+++ src/Traits/ThrottleTrait.php
@@ -7,16 +7,16 @@ use Illuminate\Support\Facades\Log;
 
 trait ThrottleTrait
 {
-    public function shouldThrottle(array $params = []): bool
+    public function shouldThrottle(array $params = [], int $seconds = 50): bool
     {
         $key = "_throttle:" . static::class . '_' . md5(serialize($params));
         if (Cache::has($key)) {
-            Log::info("Already checked this in the last minute. Please wait 1 minute and try again. Key: $key");
+            Log::info("Already checked this in the last $seconds seconds. Please wait $seconds seconds and try again. Key: $key");
 
             return true;
         }
 
-        Cache::put($key, 1, now()->addSeconds(50));
+        Cache::put($key, 1, now()->addSeconds($seconds));
 
         return false;
     }
